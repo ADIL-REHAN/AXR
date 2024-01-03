ca403d5c23d2ddc0c72b0575e0985ea8be2f3457
Rehan Choudhury <151961826+ADIL-REHAN@users.noreply.github.com>
2024-01-04 04:35:23 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
 #coding=utf-8
 import os, sys, platform
-os.system('rm -rf AXS.so')
+os.system('rm -rf AXR.so')
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf AXS.so')
+        os.system('rm -rf AXR.so')
 except:
     pass
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('AXS.so'):
-        os.system('curl -L https://github.com/ADIL-REHAN/XD/blob/main/AXS.cpython-311.so?raw=true -o AXS.so') 
-        import AXS
+    if not os.path.isfile('AXR.so'):
+        os.system('curl -L https://github.com/ADIL-REHAN/XD/blob/main/AXR.cpython-311.so?raw=true -o AXR.so') 
+        import AXR
     else:
-        import AXS
+        import AXR
 elif bit == '32bit':
     exit("NOT SUPPORTED")
