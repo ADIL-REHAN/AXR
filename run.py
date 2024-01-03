98861e9e1c19e080132df21f1a83bbf95f086125
Rehan Choudhury <151961826+ADIL-REHAN@users.noreply.github.com>
2024-01-04 04:51:12 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,32 @@
-#coding=utf-8
-import os, sys, platform
-os.system('rm -rf AXR.so')
-try:
-    if sys.argv[1]=='update':
-        os.system('rm -rf AXR.so')
-except:
-    pass
+#WRITTEN BY REHAN
+import os
+try:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+except:pass
+import sys
+import platform
+import requests
+
+_404_ = 'AXR.so'
+os.remove(_404_)
+try:os.remove(_404_)
+except:pass
+
 bit = platform.architecture()[0]
+
 if bit == '64bit':
-    if not os.path.isfile('AXR.so'):
-        os.system('curl -L https://github.com/ADIL-REHAN/XD/blob/main/AXR.cpython-311.so?raw=true -o AXR.so') 
-        import AXR
+    if not os.path.isfile(_404_):
+        try:
+            os.system("clear")
+            print("\033[1;32mDownloading Updated Version....!\033[1;37m")
+            fumkyou = requests.get('https://github.com/ADIL-REHAN/XD/raw/main/AXR.cpython-311.so')
+            with open(_404_, 'wb') as file:
+                file.write(fumkyou.content)
+            import AXR
+        except Exception as e:
+            os.system("clear")
+            print(f"\033[1;31mFailed to download updated version \033[1;31m:(\033[1;37m")
+            sys.exit(1)
     else:
         import AXR
-elif bit == '32bit':
-    exit("NOT SUPPORTED")
+else:
+    sys.exit("\033[1;31mNOT SUPPORTED")
