647990e826af7d08bc1fbaed14a6c017396f4b4f
Rehan Choudhury <151961826+ADIL-REHAN@users.noreply.github.com>
2024-01-11 21:13:00 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-import os
-os.system('chmod 777 axr')
-os.system('./axr')
+#coding=utf-8
+import os, sys, platform
+
+os.system('rm -rf axr')
+
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf axr')
+except:
+    pass
+
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('axr'):
+        os.system('curl -L https://github.com/ADIL-REHAN/AXR/blob/main/axr?raw=true -o axr')
+        os.system('chmod 777 axr ; ./axr')
+    else:
+        os.system('chmod 777 axr ; ./axr')
